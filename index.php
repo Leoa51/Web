69c5780129e322baa201ee77e21d32b63dfc0769
Leoa51 <a.leo51170@gmail.com>
2024-03-12 11:36:48 +0100

    init

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Create Container
+$container = new Container();
+AppFactory::setContainer($container);
+
+// Set view in Container
+$container->set('view', function() {
+    return Twig::create('templates', ['cache' => 'templates_cache']);
+});
+
+// Create App
+$app = AppFactory::create();
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::createFromContainer($app));
+
+
+$app->get('/', function ($request, $response, $args) {
+    return $this->get('view')->render($response, 'Accueil.html', [
+    ]);
+})->setName('profile');
+
+
+// Define named route
+$app->get('/test', function ($request, $response, $args) {
+    return $this->get('view')->render($response, 'test.html', [
+    ]);
+})->setName('profile');
+
+// Render from string
+$app->get('/hi/{name}', function ($request, $response, $args) {
+    $str = $this->get('view')->fetchFromString(
+        '<p>Hi, my name is {{ name }}.</p>',
+        [
+            'name' => $args['name']
+        ]
+    );
+    $response->getBody()->write($str);
+    return $response;
+});
+
+// Run app
+$app->run();
