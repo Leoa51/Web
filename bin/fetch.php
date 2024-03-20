af19c998a940ba5c6338fb0a2152a1b0b662c993
Leoa51 <a.leo51170@gmail.com>
2024-03-20 14:22:04 +0100

    api

--- bin/fetch.php
+++ bin/fetch.php
@@ -4,5 +4,3 @@ require_once __DIR__ . "/../doctrine/bootstrap.php";
 $query = $entityManager->createQuery('SELECT u FROM Entity\User u WHERE u.ID_User = 1');
 $users = $query->getResult();
 echo $users[0]->getLastName();
-
-
